f3bca2f20408aafc1eb83adcbcea2d6959c57ac4
Crush on Study <61110237+bomlang@users.noreply.github.com>
2023-01-29 23:29:23 +0900

    23.01.29 update
    nomard Challenge

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
-const images = ["beatles.jpg", "cats.jpg", "cats2.jpg"];
+const images = ["car.jpg", "weather.jpg", "weather.jpg", "house.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
-
+bgImage.className = "mainBg";
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
--- js/weather.js
+++ js/weather.js
@@ -1,19 +1,21 @@
-const API_KEY = "d6f0d3f1e50925e679503733041701";
-/// This is fake API_KEY. No wonder it doesn't work. /// 
+const API_KEY = "d6f0bd3f1e5c0925e679503733041706";
+/// This is fake API_KEY. No wonder it doesn't work. ///
 
-function geoSuccess(position){
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response => response.json()).then((data) => {
-        const city = document.querySelector("#weather span:first-child");
-        const weather = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+function geoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 
 function geoError() {
-    alert("Can't find you. No weather for you.");
+  alert("Can't find you. No weather for you.");
 }
 navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
